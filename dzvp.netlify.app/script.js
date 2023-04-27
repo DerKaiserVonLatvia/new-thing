b14f446f41d86882535511e050f30913ae3ce4d4
KleinerKaiser <85960973+DerKaiserVonLatvia@users.noreply.github.com>
2023-04-27 22:52:18 +0300

    main files in

---
+++ dzvp.netlify.app/script.js
@@ -0,0 +1,14 @@
+function openAboutPage(){
+    const loadInSection = document.getElementById("loadInSection");
+
+    loadInSection.className = "columns border-4 bg-bgd border-tad rounded-xl mx-4 md:mx-16 lg:mx-28 mt-20 md:mt-20 lg:mt-0 p-5 md:columns-2 lg:columns-3 xl:columns-4 space-y-4";
+
+
+
+
+
+
+    
+
+
+}
